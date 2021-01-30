ded16a88887897378e4260493ca19b94d192b16b
GitHub Action <action@github.com>
2021-01-30 19:13:54 +0000

    Template cleanup

--- src/main/kotlin/com/github/igrrik/kmmfrozeninspection/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/igrrik/kmmfrozeninspection/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.igrrik.kmmfrozeninspection.listeners
 
+import com.github.igrrik.kmmfrozeninspection.services.MyProjectService
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
---
+++ src/main/kotlin/com/github/igrrik/kmmfrozeninspection/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.igrrik.kmmfrozeninspection.services
+
+import com.github.igrrik.kmmfrozeninspection.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
