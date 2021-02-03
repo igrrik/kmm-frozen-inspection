fc8f16f0afa698452721716659ea7dab722f99fb
Igor Kokoev <igrrik@gmail.com>
2021-02-03 23:29:54 +0300

    Add check for ThreadLocal

--- src/main/kotlin/com/github/igrrik/kmmfrozeninspection/ObjectWithVariablesInspection.kt
+++ src/main/kotlin/com/github/igrrik/kmmfrozeninspection/ObjectWithVariablesInspection.kt
@@ -1,27 +1,35 @@
 package com.github.igrrik.kmmfrozeninspection
 
+import com.intellij.codeInspection.LocalInspectionToolSession
 import com.intellij.codeInspection.ProblemHighlightType
 import com.intellij.codeInspection.ProblemsHolder
 import com.intellij.psi.PsiElementVisitor
-import org.jetbrains.kotlin.idea.inspections.AbstractKotlinInspection
+import org.jetbrains.kotlin.idea.util.findAnnotation
+import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.psi.KtObjectDeclaration
 import org.jetbrains.kotlin.psi.KtVisitorVoid
 
 class ObjectWithVariablesInspection: AbstractKotlinInspection() {
 
-    override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor {
+    override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean, session: LocalInspectionToolSession): PsiElementVisitor {
         return object : KtVisitorVoid() {
             override fun visitObjectDeclaration(declaration: KtObjectDeclaration) {
                 super.visitObjectDeclaration(declaration)
-                val properties = declaration.body?.properties
-                val objectKeyword = declaration.getObjectKeyword()
-                if (objectKeyword != null && properties != null && properties.any { it.isVar }) {
-                    holder.registerProblem(
-                        objectKeyword,
-                        "Using variables will cause exception",
-                        ProblemHighlightType.WARNING
-                    )
-                }
+
+                val threadLocalName = FqName("kotlin.native.concurrent.ThreadLocal")
+                if (declaration.findAnnotation(threadLocalName) != null) return
+
+                val properties = declaration.body?.properties ?: return
+
+                properties
+                    .filter { it.isVar }
+                    .forEach { property ->
+                        holder.registerProblem(
+                            property.psiOrParent,
+                            "Add @ThreadLocal annotation to object or replace var with val",
+                            ProblemHighlightType.WARNING
+                        )
+                    }
             }
         }
     }
